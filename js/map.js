6ece221936db59fc4a7d7c56f8c061392588f5b7
Raphael Seebacher <rse@open.ch>
2017-02-22 00:08:51 +1100

    add pan and zoom

--- js/map.js
+++ js/map.js
@@ -55,4 +55,18 @@ d3.json("/data/location.json", function(error, data) {
     });
 
     d3.select(self.frameElement).style("height", height + "px");
+
+    // zoom and pan
+    var zoom = d3.behavior.zoom()
+        .on("zoom",function() {
+            g.attr("transform","translate("+ 
+                d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+            g.selectAll("circle")
+                .attr("d", path.projection(projection));
+            g.selectAll("path")  
+                .attr("d", path.projection(projection)); 
+
+      });
+
+    svg.call(zoom)
 });

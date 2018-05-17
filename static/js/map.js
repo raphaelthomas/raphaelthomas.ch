ba1a9a9521209428ab80d1576773ce925ab25fea
Raphael Seebacher <raphael@seebachers.ch>
2018-05-17 22:02:20 +1000

    Migrate map to planetaryjs

--- static/js/map.js
+++ static/js/map.js
@@ -1,56 +1,69 @@
-var size = parseInt($("#map").width()*0.5);
-var width = size,
-    height = size;
+(function() {
+  var globe = planetaryjs.planet();
+  globe.loadPlugin(autorotate(15));
+  globe.loadPlugin(drawGraticule());
+  globe.loadPlugin(planetaryjs.plugins.earth({
+    topojson: { file:   '/world-110m.json' },
+    oceans:   { fill:   'rgba(200,200,200, 0.1)' },
+    land:     { fill:   'rgba(200,200,200, 1)' },
+    borders:  { stroke: 'rgba(127,127,127, 0.25' }
+  }));
+  globe.loadPlugin(planetaryjs.plugins.pings());
 
-var projection = d3.geo.orthographic()
-    .translate([width / 2, height / 2])
-    .scale(width / 2 - 20)
-    .clipAngle(90)
-    .precision(0.6);
+  globe.projection.scale(175).translate([175, 175]).rotate([0, -10, 0]);
 
-var canvas = d3.select("#map").append("canvas")
-    .attr("width", width)
-    .attr("height", height);
+  d3.json("/location.json", function(error, data) {
+    // $("#locationText").html(jQuery.timeago(new Date(data.time * 1000)) + (data.location ? " somewhere in " + data.location : ''));
+    setInterval(function() {
+        globe.plugins.pings.add(data.coordinates[0], data.coordinates[1], { color: 'red', ttl: 2000, angle: 10 });
+    }, 2500);
+  });
 
-var c = canvas.node().getContext("2d");
+  var canvas = document.getElementById('map');
+  if (window.devicePixelRatio == 2) {
+    canvas.width = 800;
+    canvas.height = 800;
+    context = canvas.getContext('2d');
+    context.scale(2, 2);
+  }
+  globe.draw(canvas);
 
-var path = d3.geo.path()
-    .projection(projection)
-    .context(c);
+  function drawGraticule() {
+    return function(planet) {
+      planet.onDraw(function() {
+        planet.withSavedContext(function(context) {
+          var graticule = d3.geo.graticule();
+          context.beginPath();
+          planet.path.context(context)(graticule());
+          context.strokeStyle = "rgba(0,0,0,0.25)";
+          context.lineWidth = "0.25";
+          context.stroke();
+        });
+      });
+    };
+  };
 
-d3.json("/world-110m.json", function(error, world) {
-    if (error) throw error;
-    updateLocation(world);
-});
-
-function updateLocation(world) {
-    d3.json("/location.json", function(error, data) {
-        $("#locationText").html(jQuery.timeago(new Date(data.time * 1000)) + (data.location ? " somewhere in " + data.location : ''));
-
-        var land = topojson.feature(world, world.objects.land);
-        var countries = topojson.feature(world, world.objects.countries).features;
-        var graticule = d3.geo.graticule();
-
-        (function transition() {
-            d3.transition()
-                .duration(2500)
-                .tween("rotate", function() {
-                    var r = d3.interpolate(projection.rotate(), [-data.coordinates[0], -data.coordinates[1]]);
-                    return function(t) {
-                        projection.rotate(r(t));
-                        c.clearRect(0, 0, width, height);
-                        c.fillStyle   = "#fdfdfd", c.beginPath(), path({type: "Sphere"}), c.fill();
-                        c.strokeStyle = "rgba(0,0,0,0.25)", c.lineWidth = 0.25, c.beginPath(), path(graticule()), c.stroke();
-                        c.fillStyle   = "rgba(127,127,127,0.5)", c.beginPath(), path(land), c.fill();
-                        // c.strokeStyle = "rgba(0,0,0,0.25)", c.lineWidth = 0.25, c.beginPath(), path(borders), c.stroke();
-                        c.fillStyle = "rgba(255,0,0,0.5)", c.beginPath(), c.arc(size/2, size/2, 2, 0, 2*Math.PI, false), c.fill();
-// path({type: "Point", coordinates: [data.coordinates[0],data.coordinates[1]]}), c.fill();
-                    };
-                })
-                .transition()
-                .each("end", transition);
-        })();
-    });
-}
-
-d3.select(self.frameElement).style("height", height + "px");
+  function autorotate(degPerSec) {
+    return function(planet) {
+      var lastTick = null;
+      var paused = false;
+      planet.plugins.autorotate = {
+        pause:  function() { paused = true;  },
+        resume: function() { paused = false; }
+      };
+      planet.onDraw(function() {
+        if (paused || !lastTick) {
+          lastTick = new Date();
+        } else {
+          var now = new Date();
+          var delta = now - lastTick;
+          var rotation = planet.projection.rotate();
+          rotation[0] += degPerSec * delta / 1000;
+          if (rotation[0] >= 180) rotation[0] -= 360;
+          planet.projection.rotate(rotation);
+          lastTick = now;
+        }
+      });
+    };
+  };
+})();
